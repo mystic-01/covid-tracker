eaee730265e0de8880e4bfe489098ef53020103f
You know who <harshitsh151@gmail.com>
2021-10-16 20:45:43 +0530

    Project initialized with create-react-app. Basic browser styling reset.

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Covid - 19</h1>
+    </div>
+  );
+};
+
+export default App;
